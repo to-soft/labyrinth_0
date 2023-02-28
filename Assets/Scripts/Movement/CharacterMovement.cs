5566b7075e1a492387be6c3aeb740f30a91a8b92
ptr <padraic@toosoft.ooo>
2023-02-27 23:58:22 -0500

    added basic simulated physics

--- Assets/Scripts/Movement/CharacterMovement.cs
+++ Assets/Scripts/Movement/CharacterMovement.cs
@@ -1,14 +1,17 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.ProBuilder;
 using Cursor = UnityEngine.Cursor;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float maxSpeed = 10f;
-    public float mouseSensitivity = 2f;
+    public float strength = 1f;
+    public float drag = 0.5f;
     public float jumpHeight = 5;
     public GameObject cameraPivot;
 
-    private float playerSpeed;
+    private float maxRotationalAcc = 2f;
+    private float maxRotationalVel = 4f;
     private float cameraRotateX = 0f;
     private bool isGrounded = false;
     private Rigidbody _rigidbody;
@@ -18,15 +21,19 @@ public class CharacterMovement : MonoBehaviour
     private float _radius;
     private float _radiusPi;
 
-	private Transform _rightWheel;
-	private Transform _leftWheel;
+	private Transform _lwheel;
+    private float _lvel;
+    private float _lacc;
+    private Transform _rwheel;
+    private float _rvel;
+    private float _racc;
 
     void Start()
     {
         // get components
-        _rightWheel = this.gameObject.transform.GetChild(0);
-        _leftWheel = this.gameObject.transform.GetChild(1);
-        
+        _lwheel = this.gameObject.transform.GetChild(1);
+        _rwheel = this.gameObject.transform.GetChild(0);
+
         _rigidbody = GetComponent<Rigidbody>();
         _collider = GetComponent<BoxCollider>();
         
@@ -56,36 +63,75 @@ public class CharacterMovement : MonoBehaviour
         // get input values for frame
         float leftInput = Input.GetAxis("Left");
         float rightInput = Input.GetAxis("Right");
-        
-        Debug.Log($"left: {leftInput} right: {rightInput}");
 
-        // just pressing right - rotate around left
-        if (leftInput == 0f)
+        _lacc = Mathf.Clamp(strength * leftInput, -maxRotationalAcc, +maxRotationalAcc);
+        _racc = Mathf.Clamp(strength * rightInput, -maxRotationalAcc, +maxRotationalAcc);
+
+        _lvel += _lacc * dt;
+        if (_lvel > 0)
         {
-            if (rightInput > 0) moveOneWheel(_rightWheel, _leftWheel);
-            if (rightInput < 0) moveOneWheel(_rightWheel, _leftWheel, -1);
+            _lvel -= drag * dt;
+            _lvel = Mathf.Clamp(_lvel, 0, +maxRotationalVel);
         }
-        // just pressing left - rotate around right
-        if (rightInput == 0)
+        if (_lvel < 0)
         {
-            if (leftInput > 0) moveOneWheel(_leftWheel, _rightWheel);
-            if (leftInput < 0) moveOneWheel(_leftWheel, _rightWheel, -1);
+            _lvel += drag * dt;
+            _lvel = Mathf.Clamp(_lvel,  -maxRotationalVel, 0);
         }
         
+        // Debug.Log($"left acc: {_lacc} vel: {_lvel}");
+
+        _rvel += _racc * dt;
+        if (_rvel > 0)
+        {
+            _rvel -= drag * dt;
+            _rvel = Mathf.Clamp(_rvel, 0, +maxRotationalVel);
+        }
+        if (_rvel < 0)
+        {
+            _rvel += drag * dt;
+            _rvel = Mathf.Clamp(_rvel,  -maxRotationalVel, 0);
+        }
+        
+        // Debug.Log($"right acc: {_racc} vel: {_rvel}");
+        
+        moveOneWheel(_lvel * dt, _lwheel, _rwheel);
+        moveOneWheel(_rvel * dt, _rwheel, _lwheel);
+        
+        
+        // Debug.Log($"rotation: {transform.rotation} lw: {_lwheel.localRotation} rw {_rwheel.rotation}");
+
+        
+        // just pressing right - rotate around left
+        // if (leftInput == 0f)
+        // {
+        //     if (rightInput > 0)
+        //     {
+        //         _racc = strength * rightInput;
+        //     }
+        //     if (rightInput < 0) moveOneWheel(_rightWheel, _leftWheel, rightInput);
+        // }
+        // // just pressing left - rotate around right
+        // if (rightInput == 0)
+        // {
+        //     if (leftInput > 0) moveOneWheel(_leftWheel, _rightWheel, leftInput);
+        //     if (leftInput < 0) moveOneWheel(_leftWheel, _rightWheel, leftInput);
+        // }
+        //
         // rotate camera according to master local rotation
         cameraPivot.transform.localRotation = Quaternion.Euler(transform.localRotation.y, 0, 0);
 
-
-        if (rightInput > 0 && leftInput > 0)
-        {
-            _rigidbody.AddForce(10f * this.transform.forward, ForceMode.Force);
-            SpeedControl();
-            Vector3 localVelocity = transform.InverseTransformDirection(_rigidbody.velocity);
-            float rotational = (localVelocity.z * dt * 180) / _radiusPi;
-            _leftWheel.Rotate(rotational, 0, 0);
-            _rightWheel.Rotate(rotational, 0, 0);
-        }
-        
+        //
+        // if (rightInput > 0 && leftInput > 0)
+        // {
+        //     _rigidbody.AddForce(10f * this.transform.forward, ForceMode.Force);
+        //     SpeedControl();
+        //     Vector3 localVelocity = transform.InverseTransformDirection(_rigidbody.velocity);
+        //     float rotational = (localVelocity.z * dt * 180) / _radiusPi;
+        //     _leftWheel.Rotate(rotational, 0, 0);
+        //     _rightWheel.Rotate(rotational, 0, 0);
+        // }
+        //
         // check if we're on the ground
         CheckGround();
         
@@ -116,30 +162,28 @@ public class CharacterMovement : MonoBehaviour
         // }
     }
 
-    private void moveOneWheel(Transform movingWheel, Transform axisWheel, float coef = 1)
+    private void moveOneWheel(float distance, Transform movingWheel, Transform axisWheel)
     {
-        float scale = (movingWheel == _rightWheel) ? -90 : 90;
-        float dt = Time.deltaTime;
-        Vector3 pivot = axisWheel.position;
-        float pivotRadius = _width / 2;
-        float bodyRotation = coef * scale * dt;
+        float bodyDir = (movingWheel == _rwheel) ? -1 : 1;
+        float bodyRotation = (bodyDir * 180 * distance) / (Mathf.PI * _width);
+
+        Vector3 pivot = axisWheel.position + (axisWheel.position - movingWheel.position);
         transform.RotateAround(pivot, Vector3.up, bodyRotation);
-        float distance = pivotRadius * (Mathf.PI / 2);
-        float wheelRotation = (coef * distance * dt * 180) / _radiusPi;
+        
+        float wheelRotation = (120 * distance) / _radiusPi;
         movingWheel.Rotate(wheelRotation, 0, 0);
     }
 
 
     private void SpeedControl()
     {
-        Vector3 flatVelocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
-
-        if (flatVelocity.magnitude > maxSpeed)
-        {
-            Vector3 limitedVelocity = flatVelocity.normalized * maxSpeed;
-            _rigidbody.velocity = new Vector3(limitedVelocity.x, _rigidbody.velocity.y, limitedVelocity.z);
-            
-        }
+        // Vector3 flatVelocity = new Vector3(_rigidbody.velocity.x, 0f, _rigidbody.velocity.z);
+        //
+        // if (flatVelocity.magnitude > maxSpeed)
+        // {
+        //     Vector3 limitedVelocity = flatVelocity.normalized * maxSpeed;
+        //     _rigidbody.velocity = new Vector3(limitedVelocity.x, _rigidbody.velocity.y, limitedVelocity.z)
+        // }
     }
 
     void CheckGround()
